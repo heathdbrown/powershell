2f754338c13f8fdcd197d0851efdbdf5c7ad34b2
Heath Brown <heathdbrown@users.noreply.github.com>
2021-12-08 19:09:26 -0600

    Create neighbordiscovery.ps1

---
+++ neighbordiscovery.ps1
@@ -0,0 +1,17 @@
+# I think I was messing with how to find my raspberry pi on my home network with this one.
+# Yep def. it basically does a port scan for all neighbors using Test-netconnection via port 22
+# Probably need to add parameters etc.
+
+$neighbors = Get-NetNeighbor -AddressFamily IPv4 | Where-Object {$_.IPAddress -like '192.168.1*'}
+
+foreach ($neighbor in $neighbors) {
+
+$ipv4_neighbor = $neighbor.IPAddress 
+
+$result = Test-NetConnection -ComputerName $ipv4_neighbor -Port 22 -ErrorAction Stop
+
+if ($result.tcptestsucceeded = 'False') { Write-Output "$ipv4_neighbor - Failed"}
+
+else {Write-Output "$ipv4_neighbor - OK"}
+
+}
