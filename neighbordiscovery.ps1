89763b49d78aeb69fa9792a07d4d1d6ebd3ee5a7
Heath Brown <heathdbrown@users.noreply.github.com>
2022-01-18 15:31:19 -0600

    Update neighbordiscovery.ps1

--- neighbordiscovery.ps1
+++ neighbordiscovery.ps1
@@ -1,6 +1,7 @@
 # I think I was messing with how to find my raspberry pi on my home network with this one.
 # Yep def. it basically does a port scan for all neighbors using Test-netconnection via port 22
 # Probably need to add parameters etc.
+# OUI lookups http://standards-oui.ieee.org/oui/oui.txt
 
 $neighbors = Get-NetNeighbor -AddressFamily IPv4 | Where-Object {$_.IPAddress -like '192.168.1*'}
 
